3c2aa9bfbb2e7380fbbe75d96f80c5987e0a4b48
gopesh880 <gopeshnanda872@gmail.com>
2026-06-05 16:15:22 +0530

    Update market_data.py

--- utils/utils/market_data.py
+++ utils/utils/market_data.py
@@ -1,1 +1,13 @@
 
+import yfinance as yf
+
+def get_price_data(ticker):
+
+    data = yf.download(
+        ticker,
+        start="2020-01-01",
+        auto_adjust=True,
+        progress=False
+    )
+
+    return data
