4516297734493e1c461a7ac86e42310d60ce0f9a
gopesh880 <gopeshnanda872@gmail.com>
2026-06-05 16:38:32 +0530

    Delete utils/utils/utils/tickers.py

--- utils/utils/utils/tickers.py
+++
@@ -1,8 +0,0 @@
-ETF_MAPPING = {
-    "Nifty 50 ETF": "NIFTYBEES.NS",
-    "Gold ETF": "GOLDBEES.NS",
-    "Bank Nifty ETF": "BANKBEES.NS",
-    "IT ETF": "ITBEES.NS",
-    "TCS": "TCS.NS",
-    "Reliance": "RELIANCE.NS"
-}
